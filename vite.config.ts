4e0faf8bf5fcfc49a11974d0729f61df145cb0ec
Lucas Souza Viegas <lucasviegas.ti@gmail.com>
2022-07-27 14:57:39 -0300

    fix: alter base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "pokemon-react",
+  base: "https://lucassvti.github.io/pokemon-react",
   plugins: [react()],
   build: {
     outDir: path.join(__dirname, "build"),
